688bf33223bd98cdb013a06e6763b3b934ec465b
Miszkas <smmisza3@gmail.com>
2026-05-10 19:04:05 +0200

    exercise 3.12 phonebook backend project - MongoDb connection with app in terminal

---
+++ Part 3/phonebook/backend/mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://MiszaSm:${password}@cluster0.z1mlyts.mongodb.net/?appName=Cluster0`;
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>",
+  );
+  process.exit(1);
+}
+
+mongoose.connect(url, { family: 4 });
+
+const phonebookSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", phonebookSchema);
+
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    console.log("phonebook:");
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
+
+if (process.argv.length === 5) {
+  const person = new Person({
+    name: name,
+    number: number,
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
