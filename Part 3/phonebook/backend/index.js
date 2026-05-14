a66f87514db61585f0ff4d4eb28b109bf1c4efb7
Miszkas <smmisza3@gmail.com>
2026-05-14 21:23:03 +0200

    exrcises 3.15 - 3.18 phonebook backend project. Also corrected vite.config file

--- Part 3/phonebook/backend/index.js
+++ Part 3/phonebook/backend/index.js
@@ -1,14 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const Person = require("./models/person");
-
+const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
 
-const app = express();
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(cors());
-app.use(express.static("dist"));
 
 morgan.token("body", (req) => {
   return JSON.stringify(req.body);
@@ -17,6 +16,16 @@ morgan.token("body", (req) => {
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
 );
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
 
 app.get("/api/persons", (req, res) => {
   Person.find({}).then((persons) => {
@@ -30,10 +39,16 @@ app.get("/info", (req, res) => {
   });
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  Person.findById(req.params.id).then((person) => {
-    res.json(person);
-  });
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (req, res) => {
@@ -53,14 +68,37 @@ app.post("/api/persons", (req, res) => {
   });
 });
 
-app.delete("/api/persons/:id", (req, res) => {
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
   const id = req.params.id;
-  Person.findByIdAndDelete(id).then(() => {
-    res.status(204).end();
-  });
+  Person.findById(id)
+    .then((person) => {
+      if (!person) {
+        return res.status(404).end();
+      }
+
+      person.number = body.number;
+      person.save().then((updatedPerson) => {
+        res.json(updatedPerson);
+      });
+    })
+    .catch((error) => next(error));
 });
 
-const PORT = process.env.PORT || 3001;
+app.delete("/api/persons/:id", (req, res, next) => {
+  const id = req.params.id;
+  Person.findByIdAndDelete(id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
