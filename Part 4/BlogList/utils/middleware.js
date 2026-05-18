5c00952d7cc186e82abb11298a42f70564fc22b2
Miszkas <smmisza3@gmail.com>
2026-05-18 21:22:22 +0200

    Exercises 4.1 - 4.2 BlogList project

---
+++ Part 4/BlogList/utils/middleware.js
@@ -0,0 +1,32 @@
+const logger = require("./logger");
+
+const requestLogger = (request, response, next) => {
+  logger.info("Method:", request.method);
+  logger.info("Path:  ", request.path);
+  logger.info("Body:  ", request.body);
+  logger.info("---");
+  next();
+};
+
+const unknownEndpoint = (request, response) => {
+  logger.info("Unknown endpoint:", request.path);
+  response.status(404).send({ error: "Unknown endpoint" });
+};
+
+const errorHandler = (error, request, response, next) => {
+  logger.error("Error:", error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "Malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+};
